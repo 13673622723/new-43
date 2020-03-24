36124b68d0072b3079844b85e287697b225c3af0
yangguang <542965904@qq.com>
2020-03-24 17:16:09 +0800

    第一天项目

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    登录页
+    <span class="iconfont iconnew"></span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+span {
+  font-size: 100px;
+  color: green;
+}
+</style>
